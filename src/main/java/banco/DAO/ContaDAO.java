b1c6ef1d2c86e478d8eefd9d1715c5fd239dd5c0
Joaquimat <joaquimmatheussantos@gmail.com>
2026-06-02 18:39:05 -0300

    atualização utilizando finally para encerrar conexão apenas no service

--- src/main/java/banco/DAO/ContaDAO.java
+++ src/main/java/banco/DAO/ContaDAO.java
@@ -1,15 +1,12 @@
 package banco.DAO;
 
-import banco.connection.ConnectionFactory;
 import banco.model.Conta;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class ContaDAO {
@@ -18,178 +15,277 @@ public class ContaDAO {
 
     public ContaDAO(Connection connection) {
         this.conn = connection;
-
     }
 
     public void salvar(Conta conta) {
+
+        PreparedStatement ps = null;
+
         try {
 
-
-            PreparedStatement ps = conn.prepareStatement("INSERT INTO conta(numero, cliente, saldo, cpf, email) VALUES (?, ?, ?, ?, ?)");
+            ps = conn.prepareStatement("INSERT INTO conta(numero, cliente, saldo, cpf, email) VALUES (?, ?, ?, ?, ?)");
 
             ps.setInt(1, conta.getNumero());
             ps.setString(2, conta.getCliente());
-            ps.setLong(3, conta.getSaldo());
+            ps.setLong(3, 0L);
             ps.setString(4, conta.getCpf());
             ps.setString(5, conta.getEmail());
 
             ps.executeUpdate();
-            ps.close();
-            conn.close();
 
         } catch (SQLException e) {
+
             throw new RuntimeException(e);
+
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
         }
     }
-
 
     public Set<Conta> listar() {
 
         Set<Conta> contas = new HashSet<>();
 
+        PreparedStatement ps = null;
+        ResultSet rs = null;
         try {
 
+            ps = conn.prepareStatement("SELECT * FROM conta");
 
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM conta");
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
 
             while (rs.next()) {
                 Conta cnt = new Conta();
-
-                int numero = rs.getInt(1);
-                String cliente = rs.getString(2);
-                Long saldo = rs.getLong(3);
-                String cpf = rs.getString(4);
-                String email = rs.getString(5);
-
-                cnt.setNumero(numero);
-                cnt.setCliente(cliente);
-                cnt.setSaldo(saldo);
-                cnt.setCpf(cpf);
-                cnt.setEmail(email);
-
+                cnt.setNumero(rs.getInt(1));
+                cnt.setCliente(rs.getString(2));
+                cnt.setSaldo(rs.getLong(3));
+                cnt.setCpf(rs.getString(4));
+                cnt.setEmail(rs.getString(5));
                 contas.add(cnt);
-
             }
-            ps.close();
-            rs.close();
-            conn.close();
-
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
         }
         return contas;
-
     }
 
     public void deletar(Conta conta) {
 
-        try {
+        PreparedStatement ps = null;
 
-            PreparedStatement ps = conn.prepareStatement("DELETE FROM conta WHERE numero = ?");
+        try {
+            ps = conn.prepareStatement("DELETE FROM conta WHERE numero = ?");
 
             ps.setInt(1, conta.getNumero());
 
             ps.executeUpdate();
-            ps.close();
-            conn.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
         }
-
     }
 
     public void atualizar(Conta conta) {
-
+        PreparedStatement ps = null;
         try {
-
-            PreparedStatement ps = conn.prepareStatement("UPDATE conta SET email = ? WHERE numero = ?");
+            ps = conn.prepareStatement("UPDATE conta SET email = ? WHERE numero = ?");
 
             ps.setString(1, conta.getEmail());
             ps.setInt(2, conta.getNumero());
 
             ps.executeUpdate();
-            ps.close();
-            conn.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
         }
     }
 
     public void depositar(Conta conta) {
 
+        PreparedStatement ps = null;
         try {
 
-            PreparedStatement ps = conn.prepareStatement("UPDATE conta SET saldo = saldo + ? WHERE numero = ?");
+            ps = conn.prepareStatement("UPDATE conta SET saldo = saldo + ? WHERE numero = ?");
 
             ps.setLong(1, conta.getSaldo());
             ps.setInt(2, conta.getNumero());
 
             ps.executeUpdate();
-            ps.close();
-            conn.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
         }
     }
 
     public void sacar(Conta conta) {
 
-        try {
+        PreparedStatement ps = null;
 
-            PreparedStatement ps = conn.prepareStatement("UPDATE conta SET saldo = saldo - ? WHERE numero = ?");
+        try {
+            ps = conn.prepareStatement("UPDATE conta SET saldo = saldo - ? WHERE numero = ?");
 
             ps.setLong(1, conta.getSaldo());
             ps.setInt(2, conta.getNumero());
 
             ps.executeUpdate();
-            ps.close();
-            conn.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
         }
     }
 
     public Conta buscarPorNumero(Conta conta) {
 
-        try {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM conta WHERE numero = ?");
+        try {
+            ps = conn.prepareStatement("SELECT * FROM conta WHERE numero = ?");
 
             ps.setInt(1, conta.getNumero());
 
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                conta.setNumero(rs.getInt(1));
+                conta.setCliente(rs.getString(2));
+                conta.setSaldo(rs.getLong(3));
+                conta.setCpf(rs.getString(4));
+                conta.setEmail(rs.getString(5));
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+        return conta;
+    }
+
+    public Set<Integer> lerNum() {
+
+        Set<Integer> contas = new HashSet<>();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            ps = conn.prepareStatement("SELECT numero FROM conta");
+
+            rs = ps.executeQuery();
 
             while (rs.next()) {
-
-                int numero = rs.getInt(1);
-                String cliente = rs.getString(2);
-                Long saldo = rs.getLong(3);
-                String cpf = rs.getString(4);
-                String email = rs.getString(5);
-
-                conta.setNumero(numero);
-                conta.setCliente(cliente);
-                conta.setSaldo(saldo);
-                conta.setCpf(cpf);
-                conta.setEmail(email);
+                contas.add(rs.getInt(1));
             }
-
-            ps.close();
-            rs.close();
-            conn.close();
-
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
         }
-        return conta;
-
+        return contas;
     }
 
+    public Set<String> lerCpf() {
+
+        Set<String> contas = new HashSet<>();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            ps = conn.prepareStatement("SELECT cpf FROM conta");
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                contas.add(rs.getString(1));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+        return contas;
+    }
 }
+
---
+++ src/main/java/banco/service/ContaService.java
@@ -0,0 +1,4 @@
+package banco.service;
+
+public class ContaService {
+}
