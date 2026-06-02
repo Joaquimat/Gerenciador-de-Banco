6344e6b7dcedd9ab770914e9d840aa8435bff5e5
Joaquimat <joaquimmatheussantos@gmail.com>
2026-06-02 18:40:09 -0300

    service de salvar conta feito(exceções de se numero da conta ou cpf ja existerem conta n é criada)

--- src/main/java/banco/AppBanco.java
+++ src/main/java/banco/AppBanco.java
@@ -3,6 +3,7 @@ package banco;
 import banco.DAO.ContaDAO;
 import banco.connection.ConnectionFactory;
 import banco.model.Conta;
+import banco.service.ContaService;
 
 import java.sql.Connection;
 import java.util.ArrayList;
@@ -17,15 +18,23 @@ public class AppBanco {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         ContaDAO contaDAO = new ContaDAO(connectionFactory.recuperarConexao());
         Conta conta = new Conta();
+        ContaService contaService = new ContaService();
 
         //Set<Conta> lista = contaDAO.listar();
 
 
-        conta.setNumero(2);
+        conta.setNumero(6);
+        conta.setCliente("joaquim");
+        conta.setCpf("12345698743");
+        conta.setEmail("bucetinhagmail");
+
+
+        contaService.salvar(conta);
+
 
         //System.out.println(lista);
 
-        System.out.println(contaDAO.buscarPorNumero(conta));
+
 
 
     }
--- src/main/java/banco/service/ContaService.java
+++ src/main/java/banco/service/ContaService.java
@@ -1,4 +1,46 @@
 package banco.service;
 
+import banco.DAO.ContaDAO;
+import banco.connection.ConnectionFactory;
+import banco.model.Conta;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ContaService {
+
+    ConnectionFactory connectionFactory = new ConnectionFactory();
+
+    public void salvar(Conta conta) {
+
+        Connection conn = connectionFactory.recuperarConexao();
+
+        try {
+
+            ContaDAO contaDAO = new ContaDAO(conn);
+
+            Integer statusNum = conta.getNumero();
+            String statusCpf = conta.getCpf();
+
+            if (contaDAO.lerNum().contains(statusNum) || contaDAO.lerCpf().contains(statusCpf)) {
+                System.out.println("ATENÇÃO: ESSA CONTA JÁ EXISTE");
+                return;
+            }
+            contaDAO.salvar(conta);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
 }
+
